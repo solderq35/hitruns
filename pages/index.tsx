c2fb52fcbe44d643d7de3f852e81b64cc167e9bc
solderq35 <solderq35@gmail.com>
2021-11-07 06:05:29 -0800

    fix index links p2#

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,10 +43,10 @@ Welcome to HitRuns
 </center>
 
 <p>
-This is a mirror for the <a href="https://speedrun.com/hitman_3" className="class3" target="_blank" rel="noreferrer">Hitman 3 speedrun.com leaderboard</a>. It is intended as a backup and/or supplement to the speedrun.com leaderboards, not as a replacement. Backups of important Hitman 3 leaderboards will be hosted here, as will guides. 
+This is a mirror for the <a href="https://speedrun.com/hitman_3" className="class4" target="_blank" rel="noreferrer">Hitman 3 speedrun.com leaderboard</a>. It is intended as a backup and/or supplement to the speedrun.com leaderboards, not as a replacement. Backups of important Hitman 3 leaderboards will be hosted here, as will guides. 
 </p>  
 <p>
-I mostly based the structure of this site from <a href="https://github.com/milankarman/HobbitSpeedruns" className="class4" target="_blank" rel="noreferrer">milankarman's Hobbit Speedruns site</a>. You can follow my progress of adapting the site structure for Hitman 3 at the <a href="https://github.com/solderq35/hitruns" className="class3" target="_blank" rel="noreferrer">HitRuns github</a>. I'm pretty amateur at coding and the original Hobbit Speedruns site wasn't designed to host leaderboards from a ton of categories, so excuse the poorly optimized code. 
+I mostly based the structure of this site from <a href="https://github.com/milankarman/HobbitSpeedruns" className="class4" target="_blank" rel="noreferrer">milankarman's Hobbit Speedruns site</a>. You can follow my progress of adapting the site structure for Hitman 3 at the <a href="https://github.com/solderq35/hitruns" className="class4" target="_blank" rel="noreferrer">HitRuns github</a>. I'm pretty amateur at coding and the original Hobbit Speedruns site wasn't designed to host leaderboards from a ton of categories, so excuse the poorly optimized code. 
 </p>
   </Layout>
 );
