0c6b0c1b14ee110c01c83c04c4771722a2f7effc
solderq35 <solderq35@gmail.com>
2022-01-13 23:11:17 -0800

    added milliseconds to applicable levels#

--- pages/h3_hokkaido_saso_m.tsx
+++ pages/h3_hokkaido_saso_m.tsx
@@ -2,7 +2,7 @@
 import { GetStaticProps } from "next"
 import { ParsedRun } from "../interfaces/leaderboard"
 import Layout from "../components/Layout"
-import LeaderboardTable from "../components/LeaderboardTableIL"
+import LeaderboardTable from "../components/LeaderboardTableILTies"
 import { Row, Col } from "react-bootstrap"
 import Dropdown from "react-bootstrap/Dropdown"
 import { requestRuns } from "../lib/h3_hokkaido_saso_m_run"
