48fbd82d9b4e2ec898aea36a388b7009355fcae7
solderq35 <solderq35@gmail.com>
2022-10-11 21:18:11 -0700

    test meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../main.scss"
 import React from "react"
+import {Helmet} from "react-helmet";
 
 import type { AppProps } from "next/app"
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>HitRuns</title>
+				<meta name="description" content="H3 Speedrun.com Mirror Site - Faster Loading Time, Mobile Friendly" />
+            </Helmet>,
     <Component {...pageProps} />
 )
 
