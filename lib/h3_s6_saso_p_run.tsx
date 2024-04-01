f2835334c9df8328e46190979600ba04c9968c41
solderq35 <solderq35@gmail.com>
2024-03-31 17:19:31 -0700

    s6 fg

---
+++ lib/h3_s6_saso_p_run.tsx
@@ -0,0 +1,32 @@
+import axios from "axios"
+import { ReqRun, ParsedRun, ReqPlayer } from "../interfaces/leaderboard"
+import { parseRuns } from "./parseRuns"
+
+export const requestRuns: any = async () => {
+    const mapcategory = "n2y6r38d"
+    const difficulty = "4qyp9g6q"
+    const rating = "21g85yxl"
+
+    const response = await axios.get(
+        "https://www.speedrun.com/api/v1/leaderboards/j1ne5891/category/" +
+            mapcategory +
+            "?var-j84eq0wn=" +
+            rating +
+            "&var-5lypzk9l=" +
+            difficulty +
+            "&var-789d3g9n=814nxkjl&embed=platforms%2Cplayers&timing=realtime_noloads",
+        {
+            headers: {
+                "Cache-Control": "max-age=300",
+            },
+        }
+    )
+
+    const { data } = response.data
+
+    const requestedRuns: ReqRun[] = data.runs
+    const requestedEmbedPlayers: ReqPlayer[] = data.players.data
+
+    const pcRuns: ParsedRun[] = parseRuns(requestedRuns, requestedEmbedPlayers)
+    return [pcRuns]
+}
